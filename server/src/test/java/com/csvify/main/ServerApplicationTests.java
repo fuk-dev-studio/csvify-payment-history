23f15d36e71eca1940a2a190afb6be895173f086
khkmgch <kohei.kamiguchi@gmail.com>
2024-01-18 09:41:30 +0900

    追加：バックエンドのSpringBootプロジェクトを新規作成

---
+++ server/src/test/java/com/csvify/main/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csvify.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
